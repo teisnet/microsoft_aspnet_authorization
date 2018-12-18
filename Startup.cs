e9d1f57b57dfdc195d8906357194b98796e13fa3
Teis Draiby <teis@teis.net>
2018-12-18 20:59:12 +0100

    Require authenticated users

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using ContactManager.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace ContactManager
 {
@@ -43,7 +45,14 @@ namespace ContactManager
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
