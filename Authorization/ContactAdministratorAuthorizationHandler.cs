10c2cb83a5be55b742e72ecc7f12a04524fa69ea
Teis Draiby <teis@teis.net>
2018-12-20 00:47:58 +0100

    Create an administrator authorization handler

---
+++ Authorization/ContactAdministratorAuthorizationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace ContactManager.Authorization
+{
+	public class ContactAdministratorAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, Contact>
+	{
+		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, Contact resource)
+		{
+			if(context.User == null)
+			{
+				return Task.CompletedTask;
+			}
+
+			// Administrators can do anything
+			if (context.User.IsInRole(Constants.ContactAdministratorsRole))
+			{
+				context.Succeed(requirement);
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+}
