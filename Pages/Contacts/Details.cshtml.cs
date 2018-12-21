6b065238ee0c92c8fe4adf4ec22e573a421cde72
Teis Draiby <teis@teis.net>
2018-12-21 02:58:51 +0100

    Update Details

--- Pages/Contacts/Details.cshtml.cs
+++ Pages/Contacts/Details.cshtml.cs
@@ -7,34 +7,68 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContactManager.Data;
 using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ContactManager.Authorization;
 
 namespace ContactManager.Pages.Contacts
 {
-    public class DetailsModel : PageModel
+    public class DetailsModel : DI_BasePageModel
     {
-        private readonly ContactManager.Data.ApplicationDbContext _context;
-
-        public DetailsModel(ContactManager.Data.ApplicationDbContext context)
+        public DetailsModel(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
         public Contact Contact { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Contact = await _context.Contact.FirstOrDefaultAsync(m => m.ContactId == id);
+            Contact = await Context.Contact.FirstOrDefaultAsync(c => c.ContactId == id);
 
             if (Contact == null)
             {
                 return NotFound();
             }
+
+            var isAuthorized = User.IsInRole(Constants.ContactManagersRole) || User.IsInRole(Constants.ContactAdministratorsRole);
+
+            var currentUserId = UserManager.GetUserId(User);
+
+            if (!isAuthorized && currentUserId != Contact.OwnerID && Contact.Status != ContactStatus.Approved)
+            {
+                return new ChallengeResult();
+            }
+
             return Page();
+        }
+
+
+        public async Task<IActionResult> OnPostAsync(int id, ContactStatus status)
+        {
+            var contact = await Context.Contact.FirstOrDefaultAsync(m => m.ContactId == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            var contactOperation = (status == ContactStatus.Approved)
+                                    ? ContactOperations.Approve
+                                    : ContactOperations.Reject;
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(User, contact, contactOperation);
+
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            contact.Status = status;
+            Context.Contact.Update(contact);
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
